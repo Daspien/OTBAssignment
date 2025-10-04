6d039295f06a4bb4cf0c66f5e7d26dbe2d333929
Tomasz Zyntek <tzyntek@o2.pl>
2025-10-04 14:56:52 +0200

    Add some units tests, fix data

--- OTBAssignment.UnitTests/HolidaySearchUnitTests.cs
+++ OTBAssignment.UnitTests/HolidaySearchUnitTests.cs
@@ -45,7 +45,7 @@ namespace OTBAssignment.UnitTests
         }
 
         [Test]
-        public async Task HolidaySearch_AskingForAnyAirport_ShouldReturnBestResultFirst()
+        public async Task HolidaySearch_WhenAskingForAnyAirport_ShouldReturnBestResultFirst()
         {
             //Arrange
             var flightProvider = new JsonFlightsProvider();
@@ -60,6 +60,75 @@ namespace OTBAssignment.UnitTests
             result.Should().HaveCount(1);
             result.First().Flight.Id.Should().Be(7);
             result.First().Hotel.Id.Should().Be(6);
+        }
+
+        [Test]
+        public async Task HolidaySearch_WhenAskingForAnyNotCompatibleData_ShouldReturnEmptyResult()
+        {
+            //Arrange
+            var flightProvider = new JsonFlightsProvider();
+            var hotelProvider = new JsonHotelProvider();
+            var holidaySearch = new HolidaySearch(flightProvider, hotelProvider);
+
+            //Act
+            var result = await holidaySearch.GetResults(Airport.ANY, Airport.LPA, new DateTime(2025, 11, 10), 14);
+
+            //Assert
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public async Task HolidaySearch_WhenHavingMultipleOptionsFromSameAirport_ShouldReturnBestResultFirst()
+        {
+            //Arrange
+            var flightProvider = new JsonFlightsProvider();
+            var hotelProvider = new JsonHotelProvider();
+            var holidaySearch = new HolidaySearch(flightProvider, hotelProvider);
+
+            //Act
+            var result = await holidaySearch.GetResults(Airport.MAN, Airport.ANY, new DateTime(2023, 07, 01), 7);
+
+            //Assert
+            result.Should().NotBeEmpty();
+            result.Should().HaveCount(2);
+            result.First().Flight.Id.Should().Be(2);
+            result.First().Hotel.Id.Should().Be(9);
+        }
+
+        [Test]
+        public async Task HolidaySearch_WhenLookingForAnyAirports_ShouldReturnBestResultFirst()
+        {
+            //Arrange
+            var flightProvider = new JsonFlightsProvider();
+            var hotelProvider = new JsonHotelProvider();
+            var holidaySearch = new HolidaySearch(flightProvider, hotelProvider);
+
+            //Act
+            var result = await holidaySearch.GetResults(Airport.ANY, Airport.ANY, new DateTime(2023, 07, 01), 7);
+
+            //Assert
+            result.Should().NotBeEmpty();
+            result.Should().HaveCount(4);
+            result.First().Flight.Id.Should().Be(11);
+            result.First().Hotel.Id.Should().Be(9);
+        }
+
+        [Test]
+        public async Task HolidaySearch_WhenLookingForHotelWithDuplicateData_ShouldPickSmallerId()
+        {
+            //Arrange
+            var flightProvider = new JsonFlightsProvider();
+            var hotelProvider = new JsonHotelProvider();
+            var holidaySearch = new HolidaySearch(flightProvider, hotelProvider);
+
+            //Act
+            var result = await holidaySearch.GetResults(Airport.ANY, Airport.PMI, new DateTime(2023, 06, 15), 14);
+
+            //Assert
+            result.Should().NotBeEmpty();
+            result.Should().HaveCount(8);
+            result.First().Flight.Id.Should().Be(6);
+            result.First().Hotel.Id.Should().Be(3);
         }
     }
 }
